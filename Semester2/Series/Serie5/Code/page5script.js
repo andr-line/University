5b90c3cefd7787468611dd26114265f4bcac71fa
andr-line <83190225+andr-line@users.noreply.github.com>
2023-03-27 13:21:00 +0000

    temporary commit cause girhub isnt working

--- Semester2/Series/Serie5/Code/page5script.js
+++ Semester2/Series/Serie5/Code/page5script.js
@@ -28,25 +28,26 @@ fetch("./data/students_flags.json")
 	
 
 // D3.js :
-function chart(data, width, height, padding = 0) {
-	const barWidth = width / data.length;
-	const totalWidth = width + 20;
+function chart(data, width, height, barPadding = 0, padding = 20) {
+    const diagramWidth = width - padding * 2;
+	const diagramHeight = height - padding * 2;
+	const barWidth = diagramWidth / data.length;
 	// scaling using the maximum value in the data: ["name", value]
     const maxValue = data.reduce((a,b) => a[1] > b[1] ? a : b)[1];
-	const scaling = height / maxValue - 10;
+	const scaling = diagramHeight / maxValue;
 
-	const svg = d3.select("body")
-		.append("svg")
-		.attr("width", totalWidth)
+	const svg = d3.select("#chart").append("svg")
+		.attr("width", width)
 		.attr("height", height)
-		.attr("viewBox", "-20, 0, " + totalWidth + ", " + height)
+		.attr("viewBox", "" + -padding + " " + -padding + " " + (width + 2 * padding) + " " + (height + 2 * padding))
+		.attr("x", 100);
 		
 	svg.selectAll("rect")
 		.data(data)
 		.join(enter => enter.append("rect")
 		    .attr("x", (e, i) => i * barWidth)
 		    .attr("y", e => height - (e[1] * scaling))
-		    .attr("width", barWidth - padding)
+		    .attr("width", barWidth - barPadding)
 		    .attr("height", e  => e[1] * scaling)
 		    .attr("fill", e => "rgb(25, 105, " + e[1] + ")"));
 
@@ -55,16 +56,16 @@ function chart(data, width, height, padding = 0) {
 		.join(enter => enter.append("text")
 		    .text(e => e[0])
 			.attr("x", (e, i) => (i + 0.5) * barWidth)
-			.attr("y", e => height - e[1] * scaling - 3)
-			.attr("transform", (e, i) => "rotate(-90, " + (i * barWidth + barWidth * 0.5) + ", " + (height - e[1] * scaling - 3) + ")" )
+			.attr("y", e => diagramHeight - e[1] * scaling - 3)
+			.attr("transform", (e, i) => "rotate(-90, " + (i * barWidth + barWidth * 0.5) + ", " + (diagramHeight - e[1] * scaling - 3) + ")" )
 			.attr("font-family", "sans-serif")
 			.attr("font-size", "11px")
-			.attr("fill", "white"));
+			.attr("fill", "gray"));
 
 
 	const yScale = d3.scaleLinear()
 		.domain([maxValue, 0])
-		.range([height-maxValue*scaling, height]);
+		.range([padding + diagramHeight - maxValue * scaling, diagramHeight+padding]);
 
 	const yAxis = d3.axisLeft(yScale)
 	.ticks(10);
