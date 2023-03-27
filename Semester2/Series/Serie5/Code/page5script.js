ef89d5e8733146fbb481f264735d5e40bec3fa68
andr-line <83190225+andr-line@users.noreply.github.com>
2023-03-27 10:01:27 +0000

    Works ecxept the scale

--- Semester2/Series/Serie5/Code/page5script.js
+++ Semester2/Series/Serie5/Code/page5script.js
@@ -30,19 +30,24 @@ fetch("./data/students_flags.json")
 // D3.js :
 function chart(data, width, height, padding = 0) {
 	const barWidth = width / data.length;
+	const totalWidth = width + 20;
+	// scaling using the maximum value in the data: ["name", value]
+    const maxValue = data.reduce((a,b) => a[1] > b[1] ? a : b)[1];
+	const scaling = height / maxValue - 10;
 
 	const svg = d3.select("body")
 		.append("svg")
-		.attr("width", width)
-		.attr("height", height);
+		.attr("width", totalWidth)
+		.attr("height", height)
+		.attr("viewBox", "-20, 0, " + totalWidth + ", " + height)
 		
 	svg.selectAll("rect")
 		.data(data)
 		.join(enter => enter.append("rect")
 		    .attr("x", (e, i) => i * barWidth)
-		    .attr("y", e => height - (e[1] * 4))
+		    .attr("y", e => height - (e[1] * scaling))
 		    .attr("width", barWidth - padding)
-		    .attr("height", e  => e[1] * 4)
+		    .attr("height", e  => e[1] * scaling)
 		    .attr("fill", e => "rgb(25, 105, " + e[1] + ")"));
 
 	svg.selectAll("text")
@@ -50,6 +55,23 @@ function chart(data, width, height, padding = 0) {
 		.join(enter => enter.append("text")
 		    .text(e => e[0])
 			.attr("x", (e, i) => (i + 0.5) * barWidth)
-			.attr("y", e => height - e[1] * 4 - 3));
+			.attr("y", e => height - e[1] * scaling - 3)
+			.attr("transform", (e, i) => "rotate(-90, " + (i * barWidth + barWidth * 0.5) + ", " + (height - e[1] * scaling - 3) + ")" )
+			.attr("font-family", "sans-serif")
+			.attr("font-size", "11px")
+			.attr("fill", "white"));
+	
+	const yAxis = d3.axisLeft(yScale)
+		.ticks(5,4n);
+
+	const yScale = d3.scaleLinear()
+		.domain([0, maxValue])
+		.range([height, 10]);
+
+	svg.append("g")
+		.attr("class", "axis")
+		.attr("transform", "translate(" + 0 + ", 0)")
+		.call(yAxis);
+
 	console.log("created bar chart");
 };
